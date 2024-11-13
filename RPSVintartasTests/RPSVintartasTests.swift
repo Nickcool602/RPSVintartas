d7c1ce6008e442096ac1bff933e158972d1de474
Nick VIntartas <nv2009123@gmail.com>
2024-11-13 11:03:43 -0600

    Initial Commit

---
+++ RPSVintartasTests/RPSVintartasTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPSVintartasTests.swift
+//  RPSVintartasTests
+//
+//  Created by NICHOLAS VINTARTAS on 11/13/24.
+//
+
+import Testing
+@testable import RPSVintartas
+
+struct RPSVintartasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
